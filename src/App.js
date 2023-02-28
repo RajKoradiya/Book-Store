6ad9f11013e8f5cefb98afa62a0d5377537eb747
RajKoradiya <rajkoradiya003@gmail.com>
2023-02-28 18:06:35 +0530

    git commit -m "BookStore"

--- src/App.js
+++ src/App.js
@@ -1,32 +1,3 @@
-// import React from "react";
-// import Home from "./pages/Home";
-// import About from "./pages/About.";
-
-
-// function App() {
-//   const [currentpage, setCurrentpage] = React.useState("Home")
-
-//   const handleChange = () => {
-//     if (currentpage === "Home") {
-//       setCurrentpage("About")
-//     }
-//     else
-//     {
-//       setCurrentpage("Home")
-//     }
-//   }
-
-
-//   return (
-//     <div>
-//       {currentpage === "Home" ? <Home /> : <About />}
-//       <button onClick={handleChange}>Change Page</button>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { Component } from 'react'
 import Home from "./pages/Home";
 import About from "./pages/About.";
